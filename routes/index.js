a9d1ec1cc56aedea180e33d77a54829446a4dfdd
Unai Rada <unairadad@gmail.com>
2020-10-30 13:36:58 -0500

    Redirect to a room when sending get request

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,13 @@
 const express = require('express');
 const router = express.Router();
 
+const util = require('../lib/utilities.js');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  //Generate a room and redirect to that room
+  res.redirect(`/${util.getRoom(3,4,3)}`);
+  //res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
