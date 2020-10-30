bb176a870cd53e6630fd73fb6b8809ec74b793f7
hyang77 <hyang77@hawk.iit.edu>
2020-10-30 14:18:16 -0500

    Create an array to store character's name

--- public/javascripts/site.js
+++ public/javascripts/site.js
@@ -1,19 +1,21 @@
 //game part
+//create a name array for tracking
+const charNameArr = ["CHANTAL","ERIC","ALEX","BOB","PAUL","FRANK","ZOE","JOE","BUBA","RITA","RICK","ANTOINE","JOHN","CHAP","EVELYN","LADY","SAMANTHA","JENNY","JAVIER","EVAN","MATHIAS","MICHAEL","HANK","VITO"];
 //create gameboard
 function generateGameboard() {
   const gameboard = document.getElementById('gameboard');
   //generate boxes for 24 characters
-  for (let i = 1; i <= 24; i++) {
+  for (let i = 0; i < charNameArr.length; i++) {
     const box = document.createElement('div');
     box.className = "board-item";
     const charImage = document.createElement('img');
     charImage.className = "board-item-image";
     //append image source from RoboHash API
-    charImage.src = `https://robohash.org/${i}?set=set4`;
+    charImage.src = `https://robohash.org/${charNameArr[i]}?set=set4`;
     const charName = document.createElement('div');
     charName.className = "board-item-name";
     //add character's name
-    const name = document.createTextNode(i);
+    const name = document.createTextNode(charNameArr[i]);
     charName.appendChild(name);
     box.appendChild(charImage);
     box.appendChild(charName);
