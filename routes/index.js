c51ed95bfa8b078345e0a9ac6efe50e9546c26ca
Unai Rada <unairadad@gmail.com>
2020-10-30 14:01:15 -0500

    Comment some lines to test https

--- routes/index.js
+++ routes/index.js
@@ -7,8 +7,9 @@ const util = require('../lib/utilities.js');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   //Generate a room and redirect to that room
-  res.redirect(`/${util.getRoom(3,4,3)}`);
-  //res.render('index', { title: 'Express' });
+  //res.redirect(`/${util.getRoom(3,4,3)}`);
+  res.render('index', { title: 'Express' });
 });
+
 
 module.exports = router;
