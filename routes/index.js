e400c062fb365d8b1b17924f46da77431dd3d156
Unai Rada <unairadad@gmail.com>
2020-10-30 19:54:47 -0500

    Redirect to a room with Google meets like format

--- routes/index.js
+++ routes/index.js
@@ -7,9 +7,12 @@ const util = require('../lib/utilities.js');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   //Generate a room and redirect to that room
-  //res.redirect(`/${util.getRoom(3,4,3)}`);
-  res.render('index', { title: 'Express' });
+  res.redirect(`/${util.getRoom(3,4,3)}`);
+  //res.render('index', { title: 'Express' });
 });
 
+router.get('/:room([a-z]{3}-[a-z]{4}-[a-z]{3})', function(req, res, next) {
+  res.render('index', { title: `room ${req.params['room']}` });
+})
 
 module.exports = router;
