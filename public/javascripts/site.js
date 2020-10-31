6eebe914e673b6bb65bb448e24cc2a14872305cb
Unai Rada <unairadad@gmail.com>
2020-10-30 22:51:41 -0500

    Comment out the default socket channel handler

--- public/javascripts/site.js
+++ public/javascripts/site.js
@@ -1,17 +1,16 @@
-'use strict';
-
 var roomSocket = io('/' + NAMESPACE);
+/*
 var socket = io.connect("/");
 
 socket.on('message', function(data){
   console.log('Connected...');
   socket.emit('connected', 'hello server!');
 });
-
+*/
 roomSocket.on('message', data => {
   console.log('Message received: ' + data);
 
-  if (data == 'User successfully connected to the roomNamespace'){
-    roomSocket.emit('connection established', "Yeah I'm here");
-}
+  //if (data == 'User successfully connected to the roomNamespace'){
+  roomSocket.emit('connected', "Yeah I'm here");
+  //}
 });
